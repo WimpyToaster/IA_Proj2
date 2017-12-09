4d3b45f6f62274a36bc921d1efbd3b52092935eb
Varios997 <Varios997@users.noreply.github.com>
2017-12-09 19:22:14 +0000

    last
    -

--- classsol.py
+++ classsol.py
@@ -1,8 +1,12 @@
 """
 Grupo 029
+
 Antonio Terra 84702
 Diogo D'Andrade 84709
+
 """
+
+
 import numpy as np
 from sklearn import neighbors, datasets, tree, linear_model
 from sklearn.model_selection import GridSearchCV
@@ -35,18 +39,10 @@ def mytraining(f,Y):
 	treeParams = {"criterion":["gini", "entropy"], "splitter":["best", "random"], "min_samples_split":[1.0, 2, 3, 4, 5], "min_samples_leaf":[1, 2, 3, 4, 5, 10], "min_weight_fraction_leaf":[0.0, 0.1, 0.5], "max_leaf_nodes":[None, 10, 100], "min_impurity_decrease":[0.0, 0.1, 0.2]}
 	neigParams = {"n_neighbors":[1,2,3,4,5,6], "weights":["uniform", "distance"], "algorithm":["auto", "ball_tree", "kd_tree", "brute"], "leaf_size":[1, 2, 5, 10, 20, 30], "p":[1, 2]}
 
-	clf = GridSearchCV(treeC, cv = 5, param_grid=treeParams)
-	clf = GridSearchCV(neigC, cv = 5, param_grid=neigParams)
+	clf = GridSearchCV(treeC, cv = 2, param_grid=treeParams)
+	clf = GridSearchCV(neigC, cv = 2, param_grid=neigParams)
 
 	clf.fit(f, Y)
-
-	print("Best parameters set found on development set:")
-	print()
-	print(clf.best_params_)
-	print()
-	print("Best score:")
-	print()
-	print(clf.best_score_)
 
 	return clf
 
--- regsol.py
+++ regsol.py
@@ -1,8 +1,12 @@
 """
 Grupo 029
+
 Antonio Terra 84702
 Diogo D'Andrade 84709
+
 """
+
+
 import numpy as np
 from sklearn import datasets, tree, linear_model, neighbors, gaussian_process
 from sklearn.gaussian_process.kernels import *
@@ -20,23 +24,24 @@ def mytraining(X,Y):
 		print("# Tuning hyper-parameters for %s" % score)
 		print()
 
-		clf1 = GridSearchCV(KernelRidge(kernel='rbf', gamma=0.1), cv=5, param_grid={"alpha": [10, 1e0, 0.1, 1e-2, 1e-3, 1e-4], "gamma": np.logspace(-2, 2, 5)})
-		clf2 = GridSearchCV(neighbors.KNeighborsRegressor(), cv=5, param_grid={"n_neighbors":[1, 2, 5], "weights":["uniform", "distance"], "algorithm":["auto", "ball_tree"], "leaf_size":[30], "p":[2], "metric":["minkowski"]})
-		clf3 = GridSearchCV(SVR(kernel='rbf', gamma=0.1), cv=5, param_grid={"C": [1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6], "gamma": np.logspace(-2, 2, 5)})
+		clf = GridSearchCV(KernelRidge(kernel='rbf', gamma=0.1), cv=5, param_grid={"alpha": [10, 1e0, 0.1, 1e-2, 1e-3, 1e-4], "gamma": np.logspace(-2, 2, 5)}, scoring="neg_mean_squared_error")
+		#clf = GridSearchCV(neighbors.KNeighborsRegressor(), cv=5, param_grid={"n_neighbors":[1, 2, 5], "weights":["uniform", "distance"], "algorithm":["auto", "ball_tree"], "leaf_size":[30], "p":[2], "metric":["minkowski"]}, scoring='neg_mean_squared_error')
+		#clf = GridSearchCV(SVR(kernel='rbf', gamma=0.1), cv=5, param_grid={"C": [1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6], "gamma": np.logspace(-2, 2, 5)}, scoring="neg_mean_squared_error")
 
-		clf3.fit(X, Y.ravel())
+		clf.fit(X, Y.ravel())
 		
 
 		print("Best parameters set found on development set:")
 		print()
-		print(clf3.best_params_)
+		print(clf.best_params_)
 		print()
 		print("Best score:")
 		print()
-		print(clf3.best_score_)
-		means = clf3.cv_results_['mean_test_score']
-		stds = clf3.cv_results_['std_test_score']
-	return clf3
+		print(clf.best_score_)
+		means = clf.cv_results_['mean_test_score']
+		stds = clf.cv_results_['std_test_score']
+
+	return clf
 
 def myprediction(X,reg):
 
